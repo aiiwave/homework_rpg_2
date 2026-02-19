213255e4dfc3e9c2b0d653397b7a94095e0e3c21
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-02-20 00:00:57 +0500

    создала интерфейс абстрактной фабрики для компонентов врага

---
+++ src/com/narxoz/rpg/factory/EnemyFactory.java
@@ -0,0 +1,10 @@
+package com.narxoz.rpg.factory;
+
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.loot.LootTable;
+import java.util.List;
+
+public interface EnemyFactory{
+    List<Ability> createAbilities();
+    LootTable createLootTable();
+    String createAI();}
